85f22739a3607b228360bc0926ea86eb609a889e
joydeepmscs <joydeep.mscs@gmail.com>
2021-05-18 21:37:03 +0100

    Home Page functionality implementation

--- src/screens/FoodOrderingAppController.js
+++ src/screens/FoodOrderingAppController.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../screens/home/Home";
+import Details from "../screens/details/Details";
 
 class FoodOrderingAppController extends Component {
   constructor() {
@@ -20,6 +21,23 @@ class FoodOrderingAppController extends Component {
               path="/"
               render={(props) => <Home {...props} baseUrl={this.baseUrl} />}
             />
+            <Route
+              exact
+              path="/restaurant/:id"
+              render={(props) => <Details {...props} baseUrl={this.baseUrl} />}
+            />
+          {/*   <PrivateRoute
+              exact
+              path="/profile"
+              component={Profile}
+              baseUrl={this.baseUrl}
+            />
+            <PrivateRoute
+              exact
+              path="/checkout"
+              component={Checkout}
+              baseUrl={this.baseUrl}
+            /> */}
           </Switch>
         </div>
       </Router>

b65d859e00d5285da2fe316ee6a279a0b0ebd897
joydeepmscs <joydeep.mscs@gmail.com>
2021-05-22 13:07:53 +0100

    fixed Home Page search functionality and added dummy profile page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -93,6 +93,7 @@ class Home extends Component {
         super(props);
         this.state = {
             restaurantList: [],
+            allRestaurantData: []
         };
     }
 
@@ -104,6 +105,7 @@ class Home extends Component {
             if (xhr.readyState === 4 && xhr.status === 200) {
                 that.setState({
                     restaurantList: JSON.parse(this.responseText).restaurants,
+                    allRestaurantData: JSON.parse(this.responseText).restaurants
                 });
             }
         });
@@ -114,24 +116,15 @@ class Home extends Component {
     }
 
     updateSearchRestaurant = (searchRestaurant, searchOn) => {
-        let allRestaurantData = [];
         if (searchOn) {
-            if (!this.state.isSearchOn) {
-                allRestaurantData = this.state.restaurantList;
-                this.setState({
-                    restaurantList: searchRestaurant,
-                    allRestaurantData: allRestaurantData,
-                });
-            } else {
                 this.setState({
                     ...this.state,
                     restaurantList: searchRestaurant,
                 });
-            }
+
         } else {
-            allRestaurantData = this.state.allRestaurantData;
             this.setState({
-                restaurantList: allRestaurantData,
+                restaurantList: this.state.allRestaurantData,
             });
         }
     };
